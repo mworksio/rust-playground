fd5ec05b96d48cd99b8c1653e9b9b62bc880833d
mlycore <maxwell92@126.com>
2021-09-05 20:53:32 +0800

    feat: add error handle to scrape_url

--- scrape_url/src/main.rs
+++ scrape_url/src/main.rs
@@ -1,15 +1,32 @@
 use std::fs;
 
-fn main() {
+// fn main() {
+//     let url = "https://www.rust-lang.org/";
+//     let output = "rust.md";
+
+//     println!("Fetching url: {}", url);
+//     let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+
+//     println!("Converting html to markdown");
+//     let md = html2md::parse_html(&body);
+
+//     fs::write(output, md.as_bytes()).unwrap();
+//     println!("Converted markdown has been saved in {}", output);
+// }
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://www.rust-lang.org/";
     let output = "rust.md";
 
     println!("Fetching url: {}", url);
-    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let body = reqwest::blocking::get(url)?.text()?;
 
     println!("Converting html to markdown");
     let md = html2md::parse_html(&body);
 
-    fs::write(output, md.as_bytes()).unwrap();
+    fs::write(output, md.as_bytes())?;
     println!("Converted markdown has been saved in {}", output);
+
+    Ok(())
 }
+
