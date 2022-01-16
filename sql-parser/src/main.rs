dc3b8892b832785f4d2bc02c90980550efaf33e1
liyao.miao <miao.liyao@netstars.co.jp>
2022-01-16 16:43:21 +0800

    using local sqlparser-rs as crate

--- sql-parser/src/main.rs
+++ sql-parser/src/main.rs
@@ -1,11 +1,71 @@
-use sqlparser::dialect::GenericDialect;
+#![warn(clippy::all)]
+
+use std::fs;
+use simple_logger::SimpleLogger;
+use sqlparser::dialect::*;
 use sqlparser::parser::Parser;
 
 fn main() {
-    let sql = "SELECT a, b, 123, myfunc(b) FROM table_1 WHERE a > b AND b < 100 ORDER BY a DESC, b";
-    
-    let dialect = GenericDialect {};
-    let ast = Parser::parse_sql(&dialect, sql).unwrap();
+    // SimpleLogger::new().init().unwrap(); 
+    SimpleLogger::new().with_utc_timestamps().init().unwrap();
 
-    println!("AST: {:?}", ast);
+    let filename = std::env::args().nth(1).expect(
+        r#"
+        No arguments provided!
+
+        Usage:
+        $ cargo run --example cli FILENAME.sql [--dialectname]
+
+        To print the parse results as JSON:
+        $ cargo run --feature json_example --example cli FILENAME.sql [--dialectname]
+        "#,
+    );
+
+    let dialect: Box<dyn Dialect> = match std::env::args().nth(2).unwrap_or_default().as_ref() {
+        "--ansi" => Box::new(AnsiDialect {}),
+        "--postgres" => Box::new(PostgreSqlDialect {}),
+        "--ms" => Box::new(MsSqlDialect {}),
+        "--mysql" => Box::new(MySqlDialect {}),
+        "--snowflake" => Box::new(SnowflakeDialect {}),
+        "--hive" => Box::new(HiveDialect {}),
+        "--generic" => Box::new(GenericDialect {}),
+        s => panic!("Unexpected parameter: {}", s),
+    };
+
+    println!("Parsing from file '{}' using {:?}", &filename, dialect);    
+    let contents = fs::read_to_string(&filename).unwrap_or_else(|_| panic!("Unable to read the file {}", &filename));
+    let without_bom = if contents.chars().next().unwrap() as u64 != 0xfeff {
+        contents.as_str()
+    } else {
+        let mut chars = contents.chars();
+        chars.next();
+        chars.as_str()
+    };
+
+    let parse_result = Parser::parse_sql(&*dialect, without_bom);
+    match parse_result {
+        Ok(statements) => {
+            println!(
+                "Round-trip:\n'{}'",
+                statements.iter().map(std::string::ToString::to_string).collect::<Vec<_>>().join("\n")
+            );
+
+            if cfg!(feature = "json_example") {
+                #[cfg(feature = "json_example")]
+                {
+                    let serialized = serde_json::to_string_pretty(&statements).unwrap();
+                    // let serialized = serde_json::to_string(&statements).unwrap();
+                    println!("Serialized as JSON:\n{}", serialized);
+                }
+            } else {
+                println!("Parse results:\n{:#?}", statements);
+            }
+
+            std::process::exit(0);
+        } 
+        Err(e) => {
+            println!("Error during parsing: {:?}", e);
+            std::process::exit(1);
+        }
+    }
 }
