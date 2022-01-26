3797c1a4fb9d14a85020539dda742bc524f66bcb
mlycore <maxwell92@126.com>
2022-01-26 10:57:48 +0800

    chore(http_frm): move server to single Server directory

--- http_frm/src/main.rs
+++ http_frm/src/main.rs
@@ -1,60 +1,7 @@
-#[macro_use] extern crate rocket;
-// mod server;
-// pub use server::Server;
-
-#[derive(Debug)]
-// pub struct Server {
-struct Server {
-
-}
-
-impl Server {
-    pub fn init() -> Server {
-        Server {}
-    }
-
-    async fn run(self) -> Result<(), rocket::Error> {
-        /*
-        rocket::build()
-            .mount("/version", routes![version])
-            .mount("/healthz", routes![healthz])
-            .mount("/metrics", routes![metrics])
-        */
-
-        rocket::build().mount("/", routes![version])
-        .ignite().await?
-        .launch().await
-    }
-
-    /*
-    #[launch] // could only be applied to function without parameters
-    fn play() -> _ {
-        rocket::build().mount("/version", routes![version])
-    }
-    */
-}
-
-#[get("/version")]
-fn version() -> &'static str {
-    "0.0.1"
-}
-
-#[get("/healthz")]
-fn healthz() -> &'static str {
-    "ok"
-}
-
-#[get("/metrics")]
-fn metrics() -> &'static str {
-    "{prom}"
-}
-
-fn new() -> Server {
-    Server{}
-}
+use server::Server;
 
 #[rocket::main]
 async fn main() {
-    let s: Server = new();
+    let s: Server = server::new();
     s.run().await;
 }
---
+++ http_frm/src/server/server.rs
@@ -0,0 +1,38 @@
+#[macro_use] extern crate rocket;
+
+#[derive(Debug)]
+pub struct Server {
+
+}
+
+impl Server {
+    pub fn init() -> Server {
+        Server {}
+    }
+
+    pub async fn run(self) -> Result<(), rocket::Error> {
+        rocket::build()
+            .mount("/", routes![version, healthz, metrics])
+            .ignite().await?
+            .launch().await
+    }
+}
+
+#[get("/version")]
+fn version() -> &'static str {
+    "0.0.1"
+}
+
+#[get("/healthz")]
+fn healthz() -> &'static str {
+    "ok"
+}
+
+#[get("/metrics")]
+fn metrics() -> &'static str {
+    "{prom}"
+}
+
+pub fn new() -> Server {
+    Server{}
+}
