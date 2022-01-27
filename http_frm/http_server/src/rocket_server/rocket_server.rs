36c5f995e3306187e79b5f3c8b5f7f2c95026418
mlycore <maxwell92@126.com>
2022-01-27 11:59:27 +0800

    feat(http_frm): add prom test

--- http_frm/http_server/src/rocket_server/rocket_server.rs
+++ http_frm/http_server/src/rocket_server/rocket_server.rs
@@ -1,19 +1,34 @@
 #[macro_use] extern crate rocket;
+// extern crate rocket_prometheus;
+
+pub use rocket_prometheus::PrometheusMetrics;
 
 #[derive(Debug)]
 pub struct Server {
+    // r: rocket::Rocket<rocket::Build>,
 }
 
 impl Server {
-    pub fn init(&self) -> Server {
-        Server{}
-    }
+    // pub fn init(&self) -> Server {
+    //     Server{
+    //         r: rocket::build().mount("/", routes![version, metrics, healthz]),
+    //     }
+    // }
 }
 
 impl http_server::HttpServer for Server {
     fn run(&self) {
         rocket::async_main( async {
-            rocket::build().mount("/", routes![version, metrics, healthz]).launch().await;
+            let prometheus = PrometheusMetrics::new();
+            rocket::build()
+                .mount("/", routes![version, healthz])
+                .launch().await;
+            /*
+            rocket::build().ignite()
+                .attach(prometheus.clone()) // error[E0599]: no method named `attach` found for opaque type `impl std::future::Future<Output = Result<Rocket<Ignite>, rocket::Error>>` in the current scope
+                .mount("/metrics", prometheus)
+                .launch().await;
+            */
         })
     }
 }
@@ -28,10 +43,10 @@ fn healthz() -> &'static str {
     "ok"
 }
 
-#[get("/metrics")]
-fn metrics() -> &'static str {
-    "{prom}"
-}
+// #[get("/metrics")]
+// fn metrics() -> &'static str {
+//     "{prom}"
+// }
 
 pub fn new() -> impl http_server::HttpServer {
     Server{}
