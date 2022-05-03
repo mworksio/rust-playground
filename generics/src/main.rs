88c309f0a870ade0c77eefe10daf27358df04d9c
mlycore <maxwell92@126.com>
2022-05-03 09:36:32 +0800

    add example of generics

---
+++ generics/src/main.rs
@@ -0,0 +1,41 @@
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+//     let mut largest = number_list[0];
+
+//     for number in number_list {
+//         if number > largest {
+//             largest = number;
+//         }
+//     }
+
+//     println!("largest number: {}", largest);
+// }
+
+// fn largest(list : &[i32]) -> i32 {
+//     let mut largest = list[0];
+
+//     for &item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest 
+// }
+
+// fn largest<T>(list : &[T]) -> T {
+fn largest<T: std::cmp::PartialOrd>(list : &[T]) -> T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = &item;
+        }
+    }
+    largest    
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("largest number: {}", result);
+}
